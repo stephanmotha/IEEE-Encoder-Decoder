d1b3a1e16b900ca795a4f2a6fbebe98e71c2d720
Stephan Motha <stephan.motha@mail.utoronto.ca>
2021-01-12 00:13:48 -0500

    fixed edge caases

--- decode.py
+++ decode.py
@@ -1,9 +1,9 @@
-#using string
-hi = ''
 
 def intToBin(num: int, bin: str):
+    if len(bin)>32:
+        return bin
     if num==0 and bin=='':
-        return 0
+        return '0'
 
     elif num==0:
         return bin
@@ -15,34 +15,13 @@ def intToBin(num: int, bin: str):
     return intToBin(new, bin)
 
 
-print(intToBin(263, hi)[::-1])
-
-
-#using list
-# sample = []
-# def wholeToBin(num: int, bin: list):
-#     if num==0 and bin==[]:
-#         bin.append(0)
-#
-#     if num==0:
-#         return None
-#
-#     new = num // 2
-#     addedDigit = str(num%2)
-#     bin.append(addedDigit)
-#
-#     wholeToBin(new, bin)
-#
-# sample.reverse()
-# wholeToBin(263, sample)
-# print(''.join(sample))
-
+# print(intToBin(263, hi)[::-1])
 
 def floatToBin(num: float, bin: str):
-    if len(bin)>15:
+    if len(bin)>32:
         return bin
     if num==0 and bin=='':
-        return 0
+        return '0'
 
     elif num==0:
         return bin
@@ -50,42 +29,67 @@ def floatToBin(num: float, bin: str):
     temp = num*2
     addedDigit = str(temp)[0]
     new = float(str(num * 2)[1:])
-    print(new)
+    # print(new)
     bin += addedDigit
 
     return floatToBin(new, bin)
 
-#(floatToBin(0.3, ''))
+# print(floatToBin(0.1, ''))
 
 def numToSciNot(num: float):
     wholeDecimal = ''
+
+
     if num<0:
         wholeDecimal = str(num)[1:].split(".")
     else:
         wholeDecimal = str(num).split(".")
 
+    # if -1 < num < 1:
 
-    intPortion = intToBin(int(wholeDecimal[0]), '')[::-1]
+
+    # print(wholeDecimal)
+
     floatPortion = floatToBin(float("."+wholeDecimal[1]), '')
 
-    wholeBin = (intPortion+floatPortion)
+    # print(floatPortion)
+
+    if -1 < num < 1:
+        wholeBin = floatPortion
+        exp = (wholeBin.find('1')+1)*-1
+
+        print(wholeBin[abs(exp)-1:])
+        print(exp)
+
+        return [wholeBin[abs(exp)-1:], exp]
+
+    # print(floatPortion)
+    intPortion = intToBin(int(wholeDecimal[0]), '')[::-1]
+    wholeBin = intPortion+floatPortion
+
 
     return [wholeBin, len(intPortion)-1]
 
-#print(numToSciNot(263.3))
+
 
 def sciNotToIEEE(SciNot: list, original:float):
     # 1 sign bit
     signBit = ''
-    if original>0:
+    if original>=0:
         signBit = '0'
     else:
         signBit = '1'
 
-    # 8 exp bits
-    expBias = SciNot[1]+127
+    # 8 exponent bits
+    if SciNot[1] != 0:
+        expBias = SciNot[1]+127
+    else:
+        expBias = 0
+
     expToBin = intToBin(expBias, '')[::-1]
-    print(expToBin)
+    if len(expToBin)<8:
+        expToBin = "0"*(8-len(expToBin)) + expToBin
+
 
     # 23 bit mantissa
     mantissa = SciNot[0][1:24]
@@ -94,9 +98,14 @@ def sciNotToIEEE(SciNot: list, original:float):
 
 
 def Decoder(num:float):
-    sciNot = numToSciNot(num)
-    return sciNotToIEEE(sciNot, num)
+    sciNot = numToSciNot(float(num))
 
-print(Decoder(263.3))
-#11000011100000111010011001100110
-#01000011100000111010011001100110
+    answer = sciNotToIEEE(sciNot, num)
+    if len(answer)<32:
+        return answer+(32-len(answer))*"0"
+
+    return answer
+
+print(Decoder(0))
+
+
