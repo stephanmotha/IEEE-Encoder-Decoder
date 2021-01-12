0227cc0210a3e228d9c938f96867c89f405e9224
Stephan Motha <stephan.motha@mail.utoronto.ca>
2021-01-11 19:25:27 -0500

    finished decode.py

--- decode.py
+++ decode.py
@@ -1,7 +1,7 @@
 #using string
 hi = ''
 
-def wholeToBin(num: int, bin: str):
+def intToBin(num: int, bin: str):
     if num==0 and bin=='':
         return 0
 
@@ -12,10 +12,10 @@ def wholeToBin(num: int, bin: str):
     addedDigit = str(num%2)
     bin += addedDigit
 
-    return wholeToBin(new, bin)
+    return intToBin(new, bin)
 
 
-print(wholeToBin(263, hi)[::-1])
+print(intToBin(263, hi)[::-1])
 
 
 #using list
@@ -38,8 +38,8 @@ print(wholeToBin(263, hi)[::-1])
 # print(''.join(sample))
 
 
-def decimalToBin(num: int, bin: str):
-    if len(bin)>10:
+def floatToBin(num: float, bin: str):
+    if len(bin)>15:
         return bin
     if num==0 and bin=='':
         return 0
@@ -47,13 +47,53 @@ def decimalToBin(num: int, bin: str):
     elif num==0:
         return bin
 
-
     temp = num*2
     addedDigit = str(temp)[0]
     new = float(str(num * 2)[1:])
     print(new)
     bin += addedDigit
 
-    return decimalToBin(new, bin)
+    return floatToBin(new, bin)
 
-print(decimalToBin(0.3, ''))
+#(floatToBin(0.3, ''))
+
+def numToSciNot(num: float):
+    wholeDecimal = ''
+    if num<0:
+        wholeDecimal = str(num)[1:].split(".")
+
+    wholeDecimal = str(num).split(".")
+    intPortion = intToBin(int(wholeDecimal[0]), '')[::-1]
+    floatPortion = floatToBin(float("."+wholeDecimal[1]), '')
+
+    wholeBin = (intPortion+floatPortion)
+
+    return [wholeBin, len(intPortion)-1]
+
+#print(numToSciNot(263.3))
+
+def sciNotToIEEE(SciNot: list, original:float):
+    # 1 sign bit
+    signBit = ''
+    if original>0:
+        signBit = '0'
+    else:
+        signBit = '1'
+
+    # 8 exp bits
+    expBias = SciNot[1]+127
+    expToBin = intToBin(expBias, '')[::-1]
+    print(expToBin)
+
+    # 23 bit mantissa
+    mantissa = SciNot[0][1:24]
+
+    return signBit+expToBin+mantissa
+
+
+
+def Decoder(num:float):
+    sciNot = numToSciNot(num)
+    return sciNotToIEEE(sciNot, num)
+
+print(Decoder(263.3))
